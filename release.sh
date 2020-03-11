e1cb8670e391236b384b449949b0beb6b54be674
yidafu <tiaozuwushi@qq.com>
2020-03-11 21:44:54 +0800

    chore: update relase.sh, npm scripts build

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@ echo "Enter release version: "
 
 read VERSION
 
-npm --git-tag-version version $VERSION # -m "release: v$VERSION"
+npm --git-tag-version version $VERSION -m "release: v$VERSION"
 
 npm publish --access public
 
