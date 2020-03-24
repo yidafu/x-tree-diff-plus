d60a1112ddd4384d66f8539bedb99bba1633571b
yidafu <tiaozuwushi@qq.com>
2020-03-24 21:37:13 +0800

    chore: publish registry npmjs.com

--- release.sh
+++ release.sh
@@ -4,6 +4,6 @@ read VERSION
 
 npm --git-tag-version version $VERSION -m "release: v$VERSION"
 
-npm publish --access public
+npm publish --access public --registry https://registry.npmjs.com/
 
 git push origin v$VERSION
