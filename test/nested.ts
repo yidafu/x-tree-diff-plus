d35712d91fe883b6c84432c60593af3b75110f3e
Fuubi <fabriparrillo@hotmail.com>
2020-05-08 18:50:52 +0200

    test: #6 add test tree 34
    <div>These are a<b>very good</b>test.</div>

--- test/nested.ts
+++ test/nested.ts
@@ -197,3 +197,80 @@ export function createTree33(): XTree<string> {
 
   });
 }
+
+
+export function createTree34(): XTree<string> {
+  // old Tree:  <div>These are a<b>very good</b>test.</div>
+
+  return new XTree<string>({
+    label: 'DIV',
+    type: NodeType.ELEMENT,
+    index: 0,
+    children: [
+      new XTree<string>({
+        label: '#TEXT-ROOT',
+        type: NodeType.ELEMENT,
+        index: 0,
+        children: [
+          new XTree({
+            type: NodeType.TEXT,
+            index: 0,
+            value: 'These',
+          }),
+          new XTree({
+            type: NodeType.TEXT,
+            index: 1,
+            value: 'are',
+          }),
+          new XTree({
+            type: NodeType.TEXT,
+            index: 2,
+            value: 'a',
+          }),
+        ],
+      }),
+      new XTree<string>(
+        {
+          label: 'B',
+          type: NodeType.ELEMENT,
+          index: 1,
+          children: [
+            new XTree<string>(
+              {
+                type: NodeType.TEXT,
+                value: 'very',
+                index: 0,
+              },
+            ),
+            new XTree<string>(
+              {
+                type: NodeType.TEXT,
+                value: 'good',
+                index: 1,
+              },
+            ),
+          ],
+        },
+      ),
+
+      new XTree<string>(
+        {
+          label: '#TEXT-ROOT',
+          type: NodeType.ELEMENT,
+          index: 2,
+          children: [
+            new XTree<string>(
+              {
+                type: NodeType.TEXT,
+                value: 'test.',
+                index: 1,
+              },
+            ),
+          ],
+        }),
+
+    ],
+
+  });
+}
+
